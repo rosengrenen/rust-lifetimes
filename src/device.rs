97b3aaf38c8470e0d87587ea2b5827fe58f557a7
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2022-08-13 23:09:05 +0200

    combine to single file

--- src/device.rs
+++
@@ -1,3 +0,0 @@
-pub struct Device<'a> {
-    id: &'a u32,
-}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,73 @@
-mod buffer;
-mod device;
-mod memory;
+pub struct Device<'a> {
+    id: &'a u32,
+}
+
+pub struct Memory<'a> {
+    device: &'a Device<'a>,
+}
+
+impl<'a> Memory<'a> {
+    pub fn new(device: &'a Device) -> Self {
+        Self { device }
+    }
+
+    pub fn map(&mut self) {}
+
+    pub fn unmap(&mut self) {}
+
+    pub fn copy_from_host<T>(&self, _slice: &[T]) {}
+}
+
+impl<'a> Drop for Memory<'a> {
+    fn drop(&mut self) {}
+}
+
+pub struct Buffer<'a> {
+    device: &'a Device<'a>,
+    memory: Memory<'a>,
+}
+
+impl<'a> Buffer<'a> {
+    pub fn new(device: &'a Device) -> Self {
+        let buffer_memory = Memory::new(device);
+
+        Self {
+            device,
+            memory: buffer_memory,
+        }
+    }
+
+    pub fn from_data<T: Copy>(device: &'a Device, _data: &[T]) -> Self {
+        let mut staging_buffer = Buffer::new(device);
+
+        {
+            let staging_buffer_memory = staging_buffer.memory_mut();
+            staging_buffer_memory.map();
+            staging_buffer_memory.unmap();
+        }
+
+        let buffer = Self::new(device);
+        staging_buffer.copy_to_buffer(&buffer);
+
+        buffer
+    }
+
+    pub fn memory(&self) -> &Memory {
+        &self.memory
+    }
+
+    pub fn memory_mut(&'a mut self) -> &mut Memory {
+        &mut self.memory
+    }
+
+    fn copy_to_buffer(&self, _dst_buffer: &Buffer) {
+        //
+    }
+}
+
+impl<'a> Drop for Buffer<'a> {
+    fn drop(&mut self) {}
+}
 
 fn main() {
     println!("Hello, world!");
